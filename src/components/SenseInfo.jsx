2732b76288a6a1e31f9fcbf2e28cb0a340c54d77
David Gabriel Gomes Fernandes <davidbiel2013@gmail.com>
2024-10-06 19:21:05 -0300

    Mexi no Tooltip

--- src/components/SenseInfo.jsx
+++ src/components/SenseInfo.jsx
@@ -3,26 +3,26 @@ import Tooltip from './Tooltip';
 
 export default function SensoryInfo ({ sensory }) {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md my-4">
+    <div className="bg-white p-6 rounded-lg shadow-md my-4 flex flex-col gap-3">
       <h2 className="text-2xl font-bold mb-4">Informações Sensoriais</h2>
-      <p className="text-lg">
+      <p className="text-lg inline-flex gap-1">
         <strong>Visual:</strong> {sensory.visual}{' '}
         <Tooltip explanation="A cor e intensidade visual do vinho." />
       </p>
-      <p className="text-lg">
+      <p className="text-lg inline-flex gap-1">
         <strong>Gustativa:</strong> {sensory.gustative}{' '}
         
         <Tooltip explanation="Sensações no paladar, como taninos, acidez, persistência." />
       </p>
-      <p className="text-lg">
+      <p className="text-lg inline-flex gap-1">
         <strong>Olfativa:</strong> {sensory.olfactory}{' '}
         <Tooltip explanation="Aromas primários, secundários e terciários percebidos no vinho." />
       </p>
-      <p className="text-lg">
+      <p className="text-lg inline-flex gap-1">
         <strong>Harmonização:</strong> Orna bem com {sensory.pairing}{' '}
         <Tooltip explanation="Sugestões de pratos que combinam com este vinho." />
       </p>
-      <p className="text-lg">
+      <p className="text-lg inline-flex gap-1">
         <strong>Temperatura de Serviço:</strong> A temperatura ideal de consumo é {sensory.servingTemperature}{' '}
         <Tooltip explanation="A temperatura ideal para servir o vinho." />
       </p>
--- src/components/TechInfo.jsx
+++ src/components/TechInfo.jsx
@@ -3,17 +3,17 @@ import Tooltip from './Tooltip';
 
 export default function TechInfo ({ technical }) {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-md my-4">
+    <div className="bg-white p-6 rounded-lg shadow-md my-4 flex flex-col gap-3">
       <h2 className="text-2xl font-bold mb-4">Informações Técnicas</h2>
-      <p className="text-lg">
+      <p className="text-lg inline-flex gap-1">
         <strong>Volume:</strong> {technical.volume}{' '}
         <Tooltip term="Volume" explanation="Quantidade de líquido na garrafa." />
       </p>
-      <p className="text-lg">
+      <p className="text-lg inline-flex gap-1">
         <strong>Graduação Alcoólica:</strong> {technical.alcohol}{' '}
         <Tooltip term="Graduação Alcoólica" explanation="Percentual de álcool no vinho." />
       </p>
-      <p className="text-lg">
+      <p className="text-lg inline-flex gap-1">
         <strong>Classificação:</strong> {technical.classification}{' '}
         <Tooltip term="Classificação" explanation="Indica se o vinho é seco, suave, doce, etc." />
       </p>
